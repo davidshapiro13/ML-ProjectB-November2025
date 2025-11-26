49178f2b46e9cf2135641900063e58f0aeb3857c
davidshapiro13 <davidshapiro@brandeis.edu>
2025-11-26 16:48:52 -0500

    added jupter

--- CollabFilterOneVectorPerItem.py
+++ CollabFilterOneVectorPerItem.py
@@ -9,7 +9,7 @@ Scroll down to __main__ to see a usage example.
 # Make sure you use the autograd version of numpy (which we named 'ag_np')
 # to do all the loss calculations, since automatic gradients are needed
 import autograd.numpy as ag_np
-from sklearn.metrics import mean_absolute_error, mean_squared_error, make_scorer
+from sklearn.metrics import mean_absolute_error, mean_squared_error, make_scorer, root_mean_squared_error, mean_absolute_error
 import matplotlib.pyplot as plt
 
 # Use helper packages
@@ -56,13 +56,12 @@ class CollabFilterOneVectorPerItem(AbstractBaseCollabFilterSGD):
         # TODO fix the lines below to have right dimensionality & values
         # TIP: use self.n_factors to access number of hidden dimensions
         self.param_dict = dict(
-            mu=ag_np.ones(1),
+            mu=ag_np.asarray([ag_np.mean(train_tuple[2])]),
             b_per_user=ag_np.ones(n_users), # FIX dimensionality
             c_per_item=ag_np.ones(n_items), # FIX dimensionality
             U=0.001 * random_state.randn(n_users, self.n_factors), # FIX dimensionality
             V=0.001 * random_state.randn(n_items, self.n_factors), # FIX dimensionality
             )
-
 
     def predict(self, user_id_N, item_id_N,
                 mu=None, b_per_user=None, c_per_item=None, U=None, V=None):
@@ -115,6 +114,7 @@ rmse_scorer = make_scorer(mean_squared_error, greater_is_better=False)
 mae_scorer = make_scorer(mean_absolute_error, greater_is_better=False)
 scoring = {'RMSE' : rmse_scorer, 'MAE' : mae_scorer}
 if __name__ == '__main__':
+
     # Load the dataset
     train_tuple, valid_tuple, test_tuple, n_users, n_items = \
         load_train_valid_test_datasets()
@@ -125,42 +125,88 @@ if __name__ == '__main__':
         n_factors=2, alpha=0.0)
     model.init_parameter_dict(n_users, n_items, train_tuple)
 
+    find_best_step(n_users, n_items, train_tuple, valid_tuple)
+
+    RMSE_train_per_K = []
+    RMSE_valid_per_K = []
+    MAE_train_per_K = []
+    MAE_valid_per_K = []
+    models = []
     #Alpha = 0
+    '''
     for K in [2, 10, 50]:
         model = CollabFilterOneVectorPerItem(
-        n_epochs=10, batch_size=1000, step_size=0.1,
+        n_epochs=20, batch_size=1000, step_size=0.01,
         n_factors=K, alpha=0.0)
         model.init_parameter_dict(n_users, n_items, train_tuple)
         model.fit(train_tuple, valid_tuple)
         train_1 = model.trace_rmse_train
         valid_1 = model.trace_rmse_valid
-        plt.plot( model.trace_epoch, model.trace_rmse_train, '.-', color='aqua', label='Training')
-        plt.plot( model.trace_epoch, model.trace_rmse_valid,  '.-', color='pink', label="Validation")
-        plt.ylabel('RMSE')
-        plt.xlabel("Epoch")
-        plt.legend(bbox_to_anchor=(1.33, 0.5)) # make legend outside plot
-        #plt.savefig('K=' + str(K) + " train-valid RMSE alpha=0.png")
-        plt.show()
+        #plt.plot( model.trace_epoch, model.trace_rmse_train, '.-', color='blue', label='Training')
+        #plt.plot( model.trace_epoch, model.trace_rmse_valid,  '.-', color='red', label="Validation")
+        #plt.ylabel('RMSE')
+        #plt.xlabel("Epoch")
+        #plt.title("K = " + str(K))
+        #plt.legend(bbox_to_anchor=(1.33, 0.5)) # make legend outside plot
+        #plt.show()
+        RMSE_train_per_K.append(model.trace_rmse_train[-1])
+        RMSE_valid_per_K.append(model.trace_rmse_valid[-1])
+        MAE_train_per_K.append(model.trace_mae_train[-1])
+        MAE_valid_per_K.append(model.trace_mae_valid[-1])
+        models.append(model)
+    best_K_RMSE_index = RMSE_valid_per_K.index(min(RMSE_valid_per_K))
+    best_K_MAE_index = MAE_valid_per_K.index(min(MAE_valid_per_K))
+    print("Best K index for RMSE", best_K_RMSE_index)
+    print("Best K index for MAE", best_K_MAE_index)
+    #Best Model is index 2, K=5
 
-        model2 = CollabFilterOneVectorPerItem(
-        n_epochs=10, batch_size=1000, step_size=0.1,
+for K in [2, 10, 50]:
+    model = CollabFilterOneVectorPerItem(
+        n_epochs=400, batch_size=1000, step_size=0.1,
         n_factors=K, alpha=0.0)
-        model2.init_parameter_dict(n_users, n_items, train_tuple)
-        model2.fit(train_tuple, test_tuple)
-        train_2 = model2.trace_rmse_train
-        test_2 = model2.trace_rmse_valid
-        plt.plot( model.trace_epoch, model2.trace_rmse_train, '.-', color='black', label='Training')
-        plt.plot( model.trace_epoch, model2.trace_rmse_valid,  '.-', color='red', label='Testing')
-        plt.ylabel('RMSE')
-        plt.xlabel("Epoch")
-        plt.legend(bbox_to_anchor=(1.33, 0.5)) # make legend outside plot
-        #plt.savefig('K=' + str(K) + " train-test RMSE alpha=0.png")
-        plt.show()
+    model.init_parameter_dict(n_users, n_items, train_tuple)
+    parameter_grid = dict()
+    parameter_grid['n_factors'] = {2, 10, 50}
+    parameter_grid['step_size'] = {1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.1}
 
-        print("TRAIN SAME", train_1 == train_2)
-        print("Valid DIFF ", valid_1 == test_2)
+    grid_searcher = GridSearchCV(
+        model,
+        parameter_grid,
+        scoring=scoring,
+        #cv=splitter,
+        refit=True
+    )
+    grid_searcher.fit(train_tuple, valid_tuple)
 
-'''
+grid_searcher.best_params_
+
+    best_model = CollabFilterOneVectorPerItem(
+        n_epochs=400, batch_size=1000, step_size=0.1,
+        n_factors=50, alpha=0.0)
+    best_model.init_parameter_dict(n_users, n_items, train_tuple)
+    best_model.fit(train_tuple, valid_tuple)
+    training_metrics = best_model.evaluate_perf_metrics(*train_tuple)
+    validation_metrics = best_model.evaluate_perf_metrics(*valid_tuple)
+    test_metrics = best_model.evaluate_perf_metrics(*test_tuple)
+
+    MAE_train = training_metrics['mae']
+    MAE_valid = validation_metrics['mae']
+    MAE_test = test_metrics['mae']
+
+    RMSE_train = training_metrics['rmse']
+    RMSE_valid = validation_metrics['rmse']
+    RMSE_test  = test_metrics['rmse']
+
+    print("MAE")
+    print("training: ", MAE_train)
+    print("validation: ", MAE_valid)
+    print("test: ", MAE_test)
+
+    print("RMSE")
+    print("training: ", RMSE_train)
+    print("validation: ", RMSE_valid)
+    print("test: ", RMSE_test)
+
     #Alpha varying
 
     model2 = CollabFilterOneVectorPerItem(
--- Part2.py
+++ Part2.py
@@ -2,19 +2,20 @@
 from surprise import Dataset, Reader, accuracy
 from surprise.model_selection import cross_validate
 from surprise.prediction_algorithms.matrix_factorization import SVD
-from sklearn.model_selection import GridSearchCV
+from surprise.model_selection.search import GridSearchCV
 from train_valid_test_loader import load_train_valid_test_datasets
 import numpy as np
 import pandas as pd
 import os
 
+train_tuple, valid_tuple, test_tuple, n_users, n_items = \
+    load_train_valid_test_datasets()
 DATA_PATH = 'data_movie_lens_100k/'
 reader1 = Reader(
     line_format='user item rating', sep=',',
     rating_scale=(1, 5), skip_lines=1)
 reader2 = Reader(rating_scale=(1, 5))
 df = pd.read_csv(DATA_PATH + 'ratings_masked_leaderboard_set.csv')
-df['rating'] = -1000000
 dev_set = Dataset.load_from_file(
   os.path.join(DATA_PATH, 'ratings_all_development_set.csv'), reader=reader1)
 training = dev_set.build_full_trainset()
@@ -28,13 +29,15 @@ parameter_grid['n_factors'] = {2, 4, 8, 16}
 parameter_grid['lr_all'] = np.logspace(-4, 4, 9)
 parameter_grid['reg_all'] = np.logspace(-4, 4, 9)
 
-grid_searcher = GridSearchCV(
-    svd,
-    parameter_grid,
-    scoring='MAE',
+#grid_searcher = GridSearchCV(
+#    svd,
+#    parameter_grid,
+#    measure=['mae'],
     #cv=splitter,
-    refit=True
-)
+#    refit=True
+#)
+
 svd.fit(training)
-yhat = svd.test(test)
-print(yhat)
+
+print(svd.test(test))
+#print(grid_searcher.best_index)
